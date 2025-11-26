e7ff32246616f5801971f6b74bd380155260ae80
hle357-hub <LENOVO@192.168.56.1>
2025-11-26 12:23:43 +0700

    sua lai thành phần trong bang

--- database/bangbosung.sql
+++ database/bangbosung.sql
@@ -3,20 +3,21 @@ use clothing_shop;
 -- bảng này chỉ khác có mỗi order_id và maDH thoi về cách tạo mấy cái khác thì hơi khác tên 1 xíu nhé
 CREATE TABLE Orders (
     order_id VARCHAR(255) PRIMARY KEY,  -- order_id là khóa chính, có thể là mã đơn hàng (ví dụ: MAU123456)
-    user_email VARCHAR(100),
+    email VARCHAR(100),
    	maKH BIGINT,					-- Email người dùng (liên kết với tài khoản)
     maNV BIGINT,
     subtotal BIGINT,                    -- Tổng tiền hàng (chưa tính phí vận chuyển)
     shipping BIGINT,                    -- Phí vận chuyển
-    total BIGINT,                       -- Tổng tiền (bao gồm phí vận chuyển)
-    created_date TIMESTAMP,             -- Ngày tạo đơn hàng
-    status VARCHAR(50),                 -- Trạng thái đơn hàng (ví dụ: "Chờ thanh toán", "Đã thanh toán")
-    shipping_address VARCHAR(255),      -- Địa chỉ giao hàng
-    phone VARCHAR(15),                  -- Số điện thoại người nhận
-    payment_method VARCHAR(50),          -- Phương thức thanh toán (VNPay, COD, ...)
+    tongTien BIGINT,                       -- Tổng tiền (bao gồm phí vận chuyển)
+    ngayTao TIMESTAMP,             -- Ngày tạo đơn hàng
+    trangThai VARCHAR(50),                 -- Trạng thái đơn hàng (ví dụ: "Chờ thanh toán", "Đã thanh toán")
+    diaChi VARCHAR(255),      -- Địa chỉ giao hàng
+    soDienThoai VARCHAR(15),                  -- Số điện thoại người nhận
+    phuongThucThanhToan VARCHAR(50),          -- Phương thức thanh toán (VNPay, COD, ...)
     FOREIGN KEY (maKH) REFERENCES KhachHang(maKH) ON DELETE CASCADE,
     FOREIGN KEY (maNV) REFERENCES NhanVien(maNV) ON DELETE SET NULL
 );
+/*
 CREATE TABLE OrderItems (
     order_id VARCHAR(255),               -- Mã đơn hàng (liên kết với bảng Orders)
     maSP BIGINT,                         -- Mã sản phẩm
@@ -26,4 +27,16 @@ CREATE TABLE OrderItems (
     PRIMARY KEY (order_id, maSP),        -- Đặt cặp (order_id, maSP) làm khóa chính
     FOREIGN KEY (order_id) REFERENCES Orders(order_id) ON DELETE CASCADE,  -- Liên kết với bảng Orders
     FOREIGN KEY (maSP) REFERENCES SanPham(maSP) ON DELETE CASCADE          -- Liên kết với bảng sản phẩm
+);
+*/
+CREATE TABLE OrderItems (
+    order_id VARCHAR(255), --
+    maSP BIGINT,--
+    soLuong INT,--
+    donGia BIGINT,
+    thanhTien BIGINT,
+    kichCo VARCHAR(50),--
+    mauSac VARCHAR(50),--
+     FOREIGN KEY (order_id) REFERENCES Orders(order_id) ON DELETE CASCADE,
+    FOREIGN KEY (maSP) REFERENCES SanPham(maSP) ON DELETE CASCADE
 );
